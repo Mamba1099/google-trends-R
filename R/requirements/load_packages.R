c0438c04962a99ade14057d5888908d704c1ee24
Mamba1099 <mambakaranja240@gmail.com>
2024-04-05 17:33:45 +0300

    refactor: create a dynamic function to load packages and check if they are already loaded

--- R/requirements/load_packages.R
+++ R/requirements/load_packages.R
@@ -1,14 +1,29 @@
-# load required packages
-loading_packages <- function() {
-  library(tidyverse)
-  library(knitr)
-  library(tinytex) # compile LaTeX Documents
-  library(moderndive)
-  library(fivethirtyeight)
-  library(httr)
-  library(utils)
-  library(styler)
-  library(shiny)
-  library(httpuv)
+
+load_required_packages <- function(packages) {
+  # Load each required package
+  for (package in packages) {
+    if (!requireNamespace(package, quietly = TRUE)) {
+      install.packages(package, dependencies = TRUE)
+    }
+    library(package, character.only = TRUE)
+  }
 }
-loading_packages()
+
+# List of required packages
+packages_to_load <- c(
+  "tidyverse",
+  "knitr",
+  "tinytex",
+  "moderndive",
+  "fivethirtyeight",
+  "httr",
+  "utils",
+  "styler",
+  "shiny",
+  "openxlsx",
+  "httpuv"
+)
+
+# Call the function to load required packages
+load_required_packages(packages_to_load)
+
