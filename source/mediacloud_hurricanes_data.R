76cc516be99bfdcb411d40ae0285608b3e5436d2
Mamba1099 <mambakaranja240@gmail.com>
2024-04-11 15:01:33 +0300

    refactor: rename path to csv file

--- source/mediacloud_hurricanes_data.R
+++ source/mediacloud_hurricanes_data.R
@@ -7,6 +7,6 @@ mediacloud_hurricane_data <- fivethirtyeight::mediacloud_hurricanes
 # Write the formatted table to a CSV file
 write.csv(
   mediacloud_hurricane_data,
-  "C:/Users/mamba/google-trends-R/csv/mediacloud_hurricane_data.csv",
+  "C:/Users/mamba/google-trends-R/csv/mediacloud_hurricanes_data.csv",
   row.names = FALSE
 )
