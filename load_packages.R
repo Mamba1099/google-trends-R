30fadd230c6feac8f4ac1ef52bd9f607dc6dd5d7
Mamba1099 <mambakaranja240@gmail.com>
2024-04-11 15:09:41 +0300

    update package library

---
+++ load_packages.R
@@ -0,0 +1,12 @@
+# load the required packages
+# you can load them one at a time 
+library(tidyverse)
+library(knitr)
+library(tinytex)
+library(moderndive)
+library(shiny)
+library(reshape2)
+library(officer)
+library(styler)
+library(utils)
+library(fivethirtyeight)
