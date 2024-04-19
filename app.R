f0326a32757a553135004efe3d0c221de8a97453
Mamba1099 <mambakaranja240@gmail.com>
2024-04-19 19:12:53 +0300

    adjust heighty of the report div and add more report features for all data types

--- app.R
+++ app.R
@@ -15,7 +15,6 @@ ui <- fluidPage(
           "mediacloud hurricanes",
           "mediacloud state",
           "mediacloud trump",
-          "tv hurricane by network",
           "tv hurricanes",
           "tv state"
         )
@@ -51,18 +50,18 @@ ui <- fluidPage(
         plotOutput("plot", width = "100%", height = "850px"),
         tags$style(type = "text/css", "#plot {margin-bottom: 50px}")
       ),
-      div(style = "font-size: 30px; animation: rainbow-glow 2s infinite; margin-top: -50px;",
-          textOutput("scatter_warning"),
-          textOutput("correlation_warning")
-          ),
+      div(
+        style = "font-size: 30px; animation: rainbow-glow 2s infinite; margin-top: -50px;",
+        textOutput("scatter_warning"),
+      ),
       div(
         textOutput("report_section"),
         style = "
-          margin-top: 30px; 
+          margin-top: 30px;
           border: 2px solid black;
           width: 610px;
           padding: 10px;
-          height: 500px;
+          height: 480px;
           border-radius: 6px;
           margin-left: -640px;
           margin-top: -500px;
@@ -107,7 +106,6 @@ server <- function(input, output, session) {
       "mediacloud hurricanes" = "csv/mediacloud_hurricanes_data.csv",
       "mediacloud state" = "csv/mediacloud_state_data.csv",
       "mediacloud trump" = "csv/mediacloud_trump_data.csv",
-      "tv hurricane by network" = "csv/tv_hurricane_by_network_data.csv",
       "tv hurricanes" = "csv/tv_hurricane_data.csv",
       "tv state" = "csv/tv_state_data.csv"
     )
@@ -124,10 +122,19 @@ server <- function(input, output, session) {
       melted_data <- melt(data, id.vars = "date")
       
       # Create separate bar graphs for each variable
-      p <- ggplot(melted_data, aes(x = date, y = value, fill = as.factor(variable))) +
-        geom_bar(width = input$bin_width, stat = "identity",) +
-        facet_wrap(~ variable, scales = "free") +
-        theme(axis.text.x = element_text(angle = 50, vjust = 0.5, hjust = 1)) +
+      p <-
+        ggplot(melted_data, aes(
+          x = date,
+          y = value,
+          fill = as.factor(variable)
+        )) +
+        geom_bar(width = input$bin_width, stat = "identity", ) +
+        facet_wrap( ~ variable, scales = "free") +
+        theme(axis.text.x = element_text(
+          angle = 50,
+          vjust = 0.5,
+          hjust = 0.5
+        )) +
         labs(fill = "Variable")
       
       print(p)
@@ -135,11 +142,11 @@ server <- function(input, output, session) {
       melted_data <- melt(data)
       ggplot(melted_data, aes(value)) +
         geom_histogram(binwidth = input$bin_width) +
-        facet_wrap(~ variable, scales = "free")
+        facet_wrap( ~ variable, scales = "free")
     } else if (input$plot_type == "Correlation Matrix") {
       if (input$data_type == "tv hurricane by network") {
         return(NULL)
-      }else {
+      } else {
         # Plot correlation matrix for numeric variables
         numeric_data <- subset(data, select = -c(date))
         corr_matrix <- cor(numeric_data)
@@ -148,11 +155,8 @@ server <- function(input, output, session) {
           scale_fill_gradient(low = "purple4", high = "orange1") +
           theme_dark()
       }
-
+      
     } else if (input$plot_type == "Scatter Matrix") {
-      if (input$data_type == "tv by hurricane by network") {
-        return (paste("Scatter Matrix cannot be plotted for this data"))
-      }
       # Plot scatter matrix for numeric variables
       numeric_data <- subset(data, select = -c(date))
       plots <- vector("list", length = ncol(numeric_data) ^ 2)
@@ -161,7 +165,8 @@ server <- function(input, output, session) {
         col_index <- ((i - 1) %% ncol(numeric_data)) + 1
         x_var <- names(numeric_data)[col_index]
         y_var <- names(numeric_data)[row_index]
-        plots[[i]] <- ggplot(data = numeric_data, aes_string(x = x_var, y = y_var)) +
+        plots[[i]] <-
+          ggplot(data = numeric_data, aes_string(x = x_var, y = y_var)) +
           geom_point() +
           geom_density_2d() +
           theme_minimal()
@@ -170,30 +175,160 @@ server <- function(input, output, session) {
     }
   })
   
-output$scatter_warning <- renderText({
-  if (input$plot_type == "Scatter Matrix" &&
-      (input$data_type == "tv hurricane by network" || input$data_type == "tv hurricanes")) {
-    "Scatter Matrix cannot be plotted for this data"
-  }
+  output$scatter_warning <- renderText({
+    if (input$plot_type == "Scatter Matrix" &&
+        (input$data_type == "tv hurricanes")) {
+      "Scatter Matrix cannot be plotted for this data"
+    }
   })
-output$correlation_warning <- renderText({
-  if (input$plot_type == "Correlation Matrix" &&
-      (input$data_type == "tv hurricane by network")) {
-    "Correlation matrix cannot be plotted for this data"
-  }
-})
-output$report_section <- renderText({
-  req(input$data_type, input$plot_type)
-  
-  report <- ""
+  output$report_section <- renderText({
+    req(input$data_type, input$plot_type)
+    
+    report <- ""
     
     if (input$data_type == "google trends") {
       if (input$plot_type == "Histogram / Bar Graph") {
-        report <- "This is the report for google trends histogram"
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Histogram / Bar Graph.")
+      } else if (input$plot_type == "Column Distribution") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Column Distribution.")
+      } else if (input$plot_type == "Correlation Matrix") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Correlation Matrix.")
+      } else if (input$plot_type == "Scatter Matrix") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Scatter Matrix.")
       }
     }
-  return(report)
-})
+    if (input$data_type == "mediacloud hurricanes") {
+      if (input$plot_type == "Histogram / Bar Graph") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Histogram / Bar Graph.")
+      } else if (input$plot_type == "Column Distribution") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Column Distribution.")
+      } else if (input$plot_type == "Correlation Matrix") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Correlation Matrix.")
+      } else if (input$plot_type == "Scatter Matrix") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Scatter Matrix.")
+      }
+    }
+    if (input$data_type == "mediacloud state") {
+      if (input$plot_type == "Histogram / Bar Graph") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Histogram / Bar Graph.")
+      } else if (input$plot_type == "Column Distribution") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Column Distribution.")
+      } else if (input$plot_type == "Correlation Matrix") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Correlation Matrix.")
+      } else if (input$plot_type == "Scatter Matrix") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Scatter Matrix.")
+      }
+    }
+    if (input$data_type == "mediacloud trump") {
+      if (input$plot_type == "Histogram / Bar Graph") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Histogram / Bar Graph.
+                ",
+                )
+      } else if (input$plot_type == "Column Distribution") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Column Distribution.")
+      } else if (input$plot_type == "Correlation Matrix") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Correlation Matrix.")
+      } else if (input$plot_type == "Scatter Matrix") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Scatter Matrix.")
+      }
+    }
+    if (input$data_type == "tv hurricanes") {
+      if (input$plot_type == "Histogram / Bar Graph") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Histogram / Bar Graph.")
+      } else if (input$plot_type == "Column Distribution") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Column Distribution.")
+      } else if (input$plot_type == "Correlation Matrix") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Correlation Matrix.")
+      } else if (input$plot_type == "Scatter Matrix") {
+        report <-
+          paste("There is no report for",
+                input$data_type,
+                "with Scatter Matrix since it can't be plotted.")
+      }
+    }
+    if (input$data_type == "tv state") {
+      if (input$plot_type == "Histogram / Bar Graph") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Histogram / Bar Graph.")
+      } else if (input$plot_type == "Column Distribution") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Column Distribution.")
+      } else if (input$plot_type == "Correlation Matrix") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Correlation Matrix.")
+      } else if (input$plot_type == "Scatter Matrix") {
+        report <-
+          paste("This is the report for",
+                input$data_type,
+                "with Scatter Matrix.")
+      }
+    }
+    
+    return(report)
+  })
 }
 
 # Run the application
